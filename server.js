ff445d3221cfae8883d307c59711cd84ab0688ba
Hsing <hsingwu27@gmail.com>
2023-06-14 15:53:06 -0700

    fixed server express middleware

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require('express'),
 app = express(),
 port= 3008,
 server = app.listen(port,()=>console.log("yo Hsing " + port))
-app.use(express.serveStatic("public"))
+app.use(express.static("public"))
